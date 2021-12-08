b60c245f38b6a0e0dc3e09406cd5a627c0572366
hulujan.lorena.viorica@gmail.com <hulujan.lorena.viorica@gmail.com>
2021-12-08 12:22:45 +0200

    Add project files.

---
+++ Data/Hulujan_Lorena_Viorica_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Hulujan_Lorena_Viorica_Lab8.Models;
+
+namespace Hulujan_Lorena_Viorica_Lab8.Data
+{
+    public class Hulujan_Lorena_Viorica_Lab8Context : DbContext
+    {
+        public Hulujan_Lorena_Viorica_Lab8Context (DbContextOptions<Hulujan_Lorena_Viorica_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Hulujan_Lorena_Viorica_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Hulujan_Lorena_Viorica_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Hulujan_Lorena_Viorica_Lab8.Models.Category> Category { get; set; }
+    }
+}
